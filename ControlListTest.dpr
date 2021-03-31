e2038cbb77cd6d8bf34f2dc4bb5c69dc3a947a97
dev@corneliusconcepts.com <corneliusdavid>
2021-03-30 23:27:46 -0700

    added VCL style, Windows10SlateGray

--- ControlListTest.dpr
+++ ControlListTest.dpr
@@ -2,13 +2,16 @@ program ControlListTest;
 
 uses
   Vcl.Forms,
-  ufrmControlListTestMain in 'ufrmControlListTestMain.pas' {frmControlListTestMain};
+  ufrmControlListTestMain in 'ufrmControlListTestMain.pas' {frmControlListTestMain},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10 SlateGray');
   Application.CreateForm(TfrmControlListTestMain, frmControlListTestMain);
   Application.Run;
 end.
